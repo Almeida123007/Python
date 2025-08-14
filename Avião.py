0af13979cd2ee9ace213ff0d19a75186f88d7b78
Almeida123007 <kamabokugonpachiro11@gmail.com>
2025-08-14 13:10:43 -0300

    Add files via upload

---
+++ Avião.py
@@ -0,0 +1,9 @@
+nome_avião = input("Digite o nome do avião: ")
+valor_avião = float(input("Digite o valor do avião: "))
+consumo_por_litro = float(input("Digite o consumo KM/L"))
+
+print("----------------------------------")
+print(f"avião   | {nome_avião}")
+print(f"Valor   | {valor_avião}")
+print(f"Consumo | {consumo_por_litro}KM/L")
+print("----------------------------------")
